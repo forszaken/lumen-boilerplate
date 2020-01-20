98ca78923db9ed87dc9c7869766a8e26ec0c0de6
Sanjar Tolibjonov <s.tolibjonov@supermicro.local>
2020-01-20 13:45:01 +0500

    Add base return type for actions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,17 +8,17 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
 
-    public function showAllUsers()
+    public function showAllUsers(): string
     {
         return response()->json(User::all());
     }
 
-    public function showOneUser($id)
+    public function showOneUser($id): string
     {
         return response()->json(User::find($id));
     }
 
-    public function create(Request $request)
+    public function create(Request $request): string
     {
         $this->validate($request, [
             'name' => 'required',
@@ -30,15 +30,20 @@ class UserController extends Controller
         return response()->json($user, 201);
     }
 
-    public function update($id, Request $request)
+    public function update($id, Request $request): string
     {
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'location' => 'required|alpha'
+        ]);
         $user = User::findOrFail($id);
         $user->update($request->all());
 
         return response()->json($user, 200);
     }
 
-    public function delete($id)
+    public function delete($id): string
     {
         User::findOrFail($id)->delete();
         return response('Deleted Successfully', 200);
