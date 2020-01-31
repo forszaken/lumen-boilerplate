8048cab53d1d110b0f8a68fc9483d6e0bc8fc0be
Sanjar Tolibjonov <s.tolibjonov@supermicro.local>
2020-01-31 15:02:35 +0500

    Fix type-hinting, delete old files.

--- backend-api/app/Http/Controllers/UserController.php
+++ backend-api/app/Http/Controllers/UserController.php
@@ -8,17 +8,17 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
 
-    public function showAllUsers()
+    public function showAllUsers(): string
     {
         return response()->json(User::all());
     }
 
-    public function showOneUser($id)
+    public function showOneUser($id): string
     {
         return response()->json(User::find($id));
     }
 
-    public function create(Request $request)
+    public function create(Request $request): string
     {
         $this->validate($request, [
             'name' => 'required',
@@ -30,7 +30,7 @@ class UserController extends Controller
         return response()->json($user, 201);
     }
 
-    public function update($id, Request $request)
+    public function update($id, Request $request): string
     {
         $this->validate($request, [
             'name' => 'required',
@@ -43,7 +43,7 @@ class UserController extends Controller
         return response()->json($user, 200);
     }
 
-    public function delete($id)
+    public function delete($id): string
     {
         User::findOrFail($id)->delete();
         return response('Deleted Successfully', 200);
